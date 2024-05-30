1d3ad210bfad0f8e06c398ecdd67bfef64815fc8
stephenmunyi <stephenmunyi075@gmail.com>
2024-05-30 16:57:07 +0300

    completed

---
+++ lib/class.py
@@ -0,0 +1,10 @@
+def fizzbuzz(number):
+    if number % 3 == 0 and number % 5 == 0:
+        return "FizzBuzz"
+    elif number % 3 == 0:
+        return "Fizz"
+    elif number % 5 == 0:
+        return "Buzz"
+    else:
+        return number
+fizzbuzz(8) 
